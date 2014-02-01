af543edf46876a2cd40ec327f8d3fec415ccc5fb
Dominic Tarr <dominic.tarr@gmail.com>
2014-02-02 04:13:12 +0700

    use tape

--- test/index.js
+++ test/index.js
@@ -1,23 +1,41 @@
-//var tape = require('tape')
+var tape = require('tape')
 
 var ecc = require('../')
 var sjcl = require('../vendor/sjcl')
 var crypto = require('crypto')
 function hash (m) {
-  return crypto.createHash('sha256').update(m).digest('hex')
+  return crypto.createHash('sha256').update(m).digest()
 }
 
-//tape('generate & sign valid keys', function (t) {
+tape('generate & sign valid keys', function (t) {
   var curve = ecc.curves.k256
   var keys = ecc.generate(curve)
-  console.log('Keys', keys)
+//  console.log('Keys', keys)
+  console.log(keys.private, keys.private.length)
   var message = hash('hello!')
-  var sig = ecc.sign(curve, keys.private, message)
-
+  var sig = ecc.sign(curve, keys, message)
   console.log('Sig', sig)
-  var valid = ecc.verify(curve, keys.public, sig, message)
-  console.log('BITS',
-    sjcl.codec.hex.fromBits(keys.public.toBits())
-  )
+  var valid = ecc.verify(curve, keys, sig, message)
   console.log('Valid?', valid)
-//})
+  t.equal(valid, true, '(correct hash, sig, key, curve ==> valid)')
+
+  //change a bit, should make the sig invalid
+  var wrong = new Buffer(message)
+  wrong[5] = 6
+  var valid = ecc.verify(curve, keys, sig, wrong)
+  t.equal(valid, false, '(wrong message, signature is invalid)')
+
+  var wrongSig = new Buffer(sig)
+  wrongSig[5] = 6
+
+  var valid = ecc.verify(curve, keys, wrongSig, message)
+  t.equal(valid, false, '(wrong message)')
+
+  var valid = ecc.verify(ecc.curves.c256, keys, sig, message)
+  t.equal(valid, false, '(wrong curve)')
+
+  var b64 = sig.toString('base64')
+  console.log(b64, b64.length, sig.length)
+
+  t.end()
+})
