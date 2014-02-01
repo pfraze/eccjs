03238339d680dd362b7573a4a8dc6804e646be13
Dominic Tarr <dominic.tarr@gmail.com>
2014-02-02 04:06:16 +0700

    make everything use buffers

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var ecc = sjcl.ecc
 function toBuffer(words) {
   var l = words.length
   var b = new Buffer(l * 4)
-  for(var i = 0; i < l; i++)
-    b.writeInt32(words[i], i*4)
+  for(var i = 0; i < l; i++) {
+    b.writeInt32BE(words[i], i*4)
+  }
   return b
 }
 
@@ -14,42 +15,53 @@ function toWords (buffer) {
   var l = buffer.length/4
   var w = Array(l)
   for(var i = 0; i < l; i++)
-    words[i] = buffer.readInt32(i*4)
+    w[i] = buffer.readInt32BE(i*4)
   return w
 }
+
+function toPoint (curve, buffer) {
+  var w = toWords(buffer)
+  var l = w.length
+  var x = sjcl.bn.fromBits(w.slice(0, l/2))
+  var y = sjcl.bn.fromBits(w.slice(l/2))
+  return new sjcl.ecc.point(curve, x, y)
+}
+
+var toBig = sjcl.bn.fromBits
 
 exports.curves = sjcl.ecc.curves
 
 exports.generate = function (curve, paranoia) {
 
-  var PRIVATE = sjcl.bn.random(curve.r, paranoia).
-  var PUBLIC = curve.G.mult(PRIVATE)
+  var _PRIVATE = sjcl.bn.random(curve.r, paranoia)
+  var PRIVATE = toBuffer(_PRIVATE.toBits())
+  var PUBLIC = toBuffer(curve.G.mult(_PRIVATE).toBits())
 
-  return { private: PRIVATE, public: PUBLIC }
+  return { private: PRIVATE, public: PUBLIC}
 }
 
 exports.sign = function (curve, key, hash, paranoia) {
   key = key.private || key
-  console.log('hydrate key', curve, key)
-  var sec = new ecc.ecdsa.secretKey(curve, key)
-  console.log('secret', sec)
-  return sec.sign(hash, paranoia)
+  var sec = new ecc.ecdsa.secretKey(curve, sjcl.bn.fromBits(toWords(key)))
+  return toBuffer(sec.sign(toWords(hash), paranoia))
 }
 
 exports.verify = function (curve, key, sig, hash) {
   key = key.public || key
-  var pub = new ecc.ecdsa.publicKey(curve, key)
   try {
-    return pub.verify(hash, sig)
+    var pub = new ecc.ecdsa.publicKey(curve, toWords(key))
+    return pub.verify(toWords(hash), toWords(sig))
   } catch (err) {
-    console.error(err)
+    console.log('errorL', err)
+    if(!/^CORRUPT/.test(''+err)) throw err
     return false
   }
 }
 
 exports.kem = function (curve, key, paranoia) {
   key = key.public || key
-  return ecc.elGamal.publicKey(curve, key).key(paranoia)
+  var pub = new ecc.ecdsa.publicKey(curve, toWords(key))
+  return ecc.elGamal.publicKey(curve, pub).key(paranoia)
 }
 
 exports.unkem = function (curve, key, kem) {
